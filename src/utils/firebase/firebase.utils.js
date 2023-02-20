ca51e81571069fa0b40863a40546f6af9e00ce15
Sezer Emer <macpro@Sezers-MacBook-Pro.local>
2023-02-20 17:24:23 +0300

    redux toolkit completed.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBiPnzhdTX8h1-gHecNUF9neHPJssxUL7U",
+  authDomain: "crwn-clothing-db-89b92.firebaseapp.com",
+  projectId: "crwn-clothing-db-89b92",
+  storageBucket: "crwn-clothing-db-89b92.appspot.com",
+  messagingSenderId: "201338259644",
+  appId: "1:201338259644:web:662c3fc1cb691dc52ce8b9"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
